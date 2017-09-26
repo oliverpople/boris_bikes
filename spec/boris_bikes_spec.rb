c1c3a06e0a92c677caebe562bf75c6d36f04c563
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-26 14:59:14 +0100

    Created empty DockingStation class

--- spec/boris_bikes_spec.rb
+++
@@ -1,6 +0,0 @@
-
-describe "DockingStation" do
-  it "Returns new instance of 'DockingStation' when initialised" do
-    expect(DockingStation.new.instance_of?).to eq "DockingStation"
-  end
-end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,7 @@
+require 'docking_station'
+
+describe DockingStation do
+  # it "Returns new instance of 'DockingStation' when initialised" do
+  #   expect(DockingStation.new.instance_of?).to eq "DockingStation"
+  # end
+end
